9f363ecee3d04689b0676d1207c59988633f4651
Aaron Anodide <greentunes@gmail.com>
2012-12-06 18:19:17 -0800

    add todos view model

---
+++ Models/TodosVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Todos.Mvc.Models
+{
+    public class TodosVM
+    {
+        List<string> Todos { get; set; }
+    }
+}
