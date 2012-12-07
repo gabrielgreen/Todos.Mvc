1f4e1e8fab0ff33954e974f3a1a38e60474dcd1f
Aaron Anodide <greentunes@gmail.com>
2012-12-07 01:12:33 -0800

    added some comments

--- Scripts/todos_ui.js
+++ Scripts/todos_ui.js
@@ -1,12 +1,16 @@
 ﻿var TodosUI = Class.create({
+
     initialize: function (params) {
         this.ajaxURL = params.addURL;
         this.setupFormKeypressHandler();
         this.showAdd();
     },
+
+    // Intercepts the ENTER keypress on the text box for adding a new item
+    // If there is non-whitespace in the text box then perform AJAX update
     setupFormKeypressHandler: function () {
         me = this;
-        theForm().keypress(function (event) {
+        this.theForm().keypress(function (event) {
             if (event.which == 13) {
                 event.preventDefault();
                 if (!String.isNullOrWhitespace(me.itemInput().val())) {
@@ -15,6 +19,10 @@
             }
         });
     },
+
+    // Performs an AJAX POST of serialized form data to an action method 
+    // which returns the content of a rendered partial view to replace
+    // the HTML of the item list
     doAjax: function (url) {
         me = this;
         jQuery.ajax(this.ajaxURL, {
@@ -26,23 +34,39 @@
             }
         });
     },
+
+    // Replaces the HTML of the item list
+    setItems: function (content) {
+        this.todoList().html(content);
+    },
+
+    // 
+    // These functions manage the text for adding a new item
+    //
+
     showAdd: function () {
         this.itemInput().removeClass('not-visible');
         this.itemInput().focus();
     },
-    setItems: function (content) {
-        this.todoList().html(content);
-    },
+
     clearInput: function () {
         this.itemInput().val('');
     },
+
+    //
+    // These functions return the jQuery object for the DOM element
+    //
+
     itemInput: function () {
         return jQuery('#input-todo');
     },
+
     todoList: function () {
         return jQuery('#container-todos');
     },
+
     theForm: function () {
         return jQuery('form');
     }
+
 });
