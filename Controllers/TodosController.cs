444af473cc39257ec19520e8a1971944d35228ed
Aaron Anodide <greentunes@gmail.com>
2012-12-06 21:29:31 -0800

    action method to add a todo and return partial view of all items

--- Controllers/TodosController.cs
+++ Controllers/TodosController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Todos.Mvc.Models;
 
 namespace Todos.Mvc.Controllers
 {
@@ -11,9 +12,31 @@ namespace Todos.Mvc.Controllers
         //
         // GET: /Todos/
 
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+
+        //
+        // POST: /Todos/Add
+
+        [HttpPost]
+        public ActionResult Add(List<string> Todos, string addTodo)
+        {
+            var viewModel = new TodosVM();
+
+            if (Todos != null)
+            {
+                viewModel.Todos.AddRange(Todos);
+            }
+
+            if (!string.IsNullOrWhiteSpace(addTodo))
+            {
+                viewModel.Todos.Add(addTodo);
+            }
+
+            return PartialView("ListPartial", viewModel);
         }
 
     }
