c5cc3379cfd14c13ff88c8a59379f4ef9be1691f
Aaron Anodide <greentunes@gmail.com>
2012-12-06 21:28:28 -0800

    js class encapsulating the js ui

---
+++ Scripts/todos_ui.js
@@ -0,0 +1,45 @@
+﻿var TodosUI = Class.create({
+    initialize: function (params) {
+        this.ajaxURL = params.addURL;
+        this.setupFormKeypressHandler();
+        this.showAdd();
+    },
+    setupFormKeypressHandler: function () {
+        me = this;
+        jQuery('form').keypress(function (event) {
+            if (event.which == 13) {
+                event.preventDefault();
+                if (!String.isNullOrWhitespace(me.itemInput().val())) {
+                    me.doAjax();
+                }
+            }
+        });
+    },
+    doAjax: function (url) {
+        me = this;
+        jQuery.ajax(this.ajaxURL, {
+            type: 'POST',
+            data: jQuery('form').serialize(),
+            success: function (data) {
+                me.setItems(data);
+                me.clearInput();
+            }
+        });
+    },
+    showAdd: function () {
+        this.itemInput().removeClass('not-visible');
+        this.itemInput().focus();
+    },
+    setItems: function (content) {
+        this.todoList().html(content);
+    },
+    clearInput: function () {
+        this.itemInput().val('');
+    },
+    itemInput: function () {
+        return jQuery('#input-todo');
+    },
+    todoList: function () {
+        return jQuery('#container-todos');
+    }
+});
